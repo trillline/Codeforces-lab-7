a8a1affaa7e0481dd5cf3215845d33c2c7df458a
trillline <vladislav1sm63@gmail.com>
2024-03-27 03:25:38 +0400

    ThirdTask
    У Евлампия большая библиотека. Каждая книга имеет в ней уникальный номер. Мы не будем сейчас рассказывать о том, по какому принципу Евлампий формирует этот номер для книги; для нас достаточно, что этот номер можно считать положительным целым числом.
    
    Друзья нередко берут у Евлампия книги, чтобы почитать их дома. А Евлампий всегда записывает, какую книгу и кому отдал. Конечно, друзья Евлампия делятся между собой впечатлениями о прочитанных книгах, и читают какие-то книги по совету друг друга.
    
    Недавно Евлампий заинтересовался, сколько разных книг из его библиотеки прочитали его друзья. Ваша задача — определить это по его записям.
    
    Ради упрощения мы не будем приводить имена друзей, которые брали ту или иную книгу: ведь к вопросу Евлампия это не имеет никакого отношения.

---
+++ ThirdTask.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include "algorithm"
+void inputVector(std::vector<int>& Books, int& n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> Books[i];
+	}
+}
+int main()
+{
+	int n; //кол-во записей Евлампия
+	std::cin >> n; //ввод
+	int count = 0; //счётчик уникальных номеров
+	std::vector<int> BooksNums(n);
+	inputVector(BooksNums, n); //вводим массив
+	std::sort(BooksNums.begin(), BooksNums.end()); //сортируем
+	for (int i = 0; i < BooksNums.size(); i++) //от 0 до сайзового значения
+	{
+		if ((BooksNums[i] != BooksNums[i + 1]) && (i != BooksNums.size())) //если след номер другой, то он новый уникальный
+		{
+			count++; //значит прибавляем
+		}
+	}
+	std::cout << count; //вывод
+	return 0;
+}
