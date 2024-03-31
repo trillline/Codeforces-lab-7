4c961596767258eadfe84ca28a4156ab1d078c1c
trillline <vladislav1sm63@gmail.com>
2024-03-31 15:08:19 +0400

    FivethTask
    Известно, что в начальный момент времени лифт в офисном здании находился на этаже #k
    . Дан список из n
     этажей, на которых лифт последовательно останавливался.
    
    Ваша задача — по заданным числам dj
     определить, сколько раз лифт проезжал (в любом направлении) ровно dj
     этажей без остановки.
    
    Важное замечание. Операция вывода данных во многих языках достаточно медленная, поэтому в задаче не требуется вывести ответ для всех возможных значений dj
    . Тем не менее, рекомендуется провести подсчёт для всех возможных dj
    , сохранив результат в списке / массиве, а затем уже вывести те значения, которые требуются.
    
    Входные данные
    В первой строке содержатся целые числа k
    , n
    , m
     (1≤k≤106,1≤n≤105,1≤m≤105)
    , k
     — номер этажа, на котором находился лифт в начальный момент времени, n
     — количество этажей в списке, m
     — количество элементов в списке dj
    .
    
    Во второй строке содержится n
     целых чисел через пробел — номера этажей, на которых последовательно останавливался лифт. Все числа — натуральные и не превосходят 106
    . Гарантируется, что каждый раз лифт перемещался хотя бы на один этаж.
    
    В третьей строке содержится m
     целых чисел dj
     (1≤dj≤106−1,j=1,2,…,m)
    .
    
    Выходные данные
    Выведите m
     чисел. Число на строке #j
     — количество раз, которое лифт проехал (в любом направлении) ровно dj
     этажей без остановки.
    
    Разделяйте числа пробелами или переводами строк.

---
+++ FivethTask.cpp
@@ -0,0 +1,41 @@
+﻿﻿#include <iostream>
+#include <vector>
+#include "algorithm"
+void inputArr(std::vector <int>& arr, int count, int k)
+{
+	int x;
+	for (int i = 0; i < count; i++)
+	{
+		std::cin >> x;
+		arr.push_back(x);
+	}
+}
+int main()
+{
+	int k, n, m; //k - начальный этаж, n - данные перемещений между этажами, m - перемещение на n этажей
+	int differenceStops;
+	int i = 0;
+	std::cin >> k >> n >> m;
+	std::vector<int> ElevatorStops, StopsMovement;
+	ElevatorStops.push_back(k); //перемещения между этажами // список перемещений на n этажей
+	inputArr(ElevatorStops, n, k);
+	inputArr(StopsMovement, m, k);
+	auto iter = std::max_element(begin(StopsMovement), end(StopsMovement));
+	std::vector <int> MovementNums(*iter + 1);
+	std::vector <int>::iterator itMove = ElevatorStops.begin();
+	while (i < n + 1)
+	{
+		if (i + 1 != n + 1)
+		{
+			differenceStops = abs(ElevatorStops[i] - ElevatorStops[i + 1]);
+			if (differenceStops < MovementNums.size())
+				MovementNums[differenceStops]++;
+		}
+		i++;
+	}
+	for (auto iterStops = StopsMovement.begin(); iterStops < StopsMovement.end(); iterStops++)
+	{
+		std::cout << MovementNums[*iterStops] << std::endl;
+	}
+	return 0;
+}
