5bc99cee07b44f7cdc71259126a401325d3df7b8
trillline <vladislav1sm63@gmail.com>
2024-03-31 15:07:08 +0400

    FourthTask
    Недавно Евлампий побывал на выставке современного искусства под открытым небом. Так получилось, что он стал одним из первых ее посетителей и получил в подарок буклет. В буклете описаны все n арт-объектов выставки, при этом каждому арт-объекту посвящен ровно один лист (информация на нем напечатана на обеих сторонах).
    
    Арт-объекты размещены вдоль длинной улицы. Они занумерованы от 1 до n и расположены именно в этом порядке. Однако составители буклета не знали, как будут размещены объекты. Поэтому порядок, в котором в буклете описаны арт-объекты, совсем другой.
    
    Евлампий решил пойти в направлении от арт-объекта #1 к арт-объекту #n. Подходя к очередному арт-объекту, он пролистывает буклет от текущего (последнего просмотренного им) листа до листа, на котором содержится описание этого очередного арт-объекта, и внимательно прочитывает его.
    
    Ваша задача — определить, сколько листов суммарно придется перевернуть Евлампию, пока он дойдет до конца выставки.
    
    Совет. Внимательно прочтите разбор примера.
    
    Входные данные
    В первой строке содержится целое число n (1 ≤ n ≤ 105) — количество арт-объектов.
    
    Во второй строке содержится n различных целых чисел s1, s2, ..., sn (1 ≤ sj ≤ n, j = 1, 2, ..., n) — порядок упоминания арт-объектов в буклете.
    
    Выходные данные
    В первой строке выведите единственное целое число — количество листов, которое придется перевернуть Евлампию.

---
+++ FourthTask.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include "algorithm"
+void inputVector(std::vector<int>& Objects, int& n) //вводим вектор
+{
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> Objects[i];
+	}
+}
+int main()
+{
+	int n; //кол-во записей Евлампия
+	long long count = 0; //подсчёт
+	bool flag = false; //флаг (показывает прочитано ли или нет)
+	std::cin >> n;
+	std::vector<int> ArtObjects(n);
+	inputVector(ArtObjects, n); //заполянем вектор
+	auto MoveIter = ArtObjects.begin(); //итератор будет по массиву последовательно двигаться
+	for (int i = 1; i <= n; i++)
+	{
+		auto iter = std::find(begin(ArtObjects), end(ArtObjects), i); //итератор поиска 
+		int diff = iter - MoveIter; //разница между двумя итераторами
+		if (diff > 0) //двигаемся по буклету назад
+		{
+			if (flag == true) //показывает прочитана ли страница
+			{
+				count--;
+				flag = false;
+			}
+			count += diff; //листаем
+			MoveIter = iter; //переприсваиваем позицию двигающемуся итератору
+		}
+		else if (diff < 0) //если меньше нуля (то есть двигаемся по буклету назад)
+		{
+			count += abs(diff) + 1; //модуль перевернувшихся листьев и +1 потому что будем читать с начала страницы
+			MoveIter = iter; //переприсваиваем 
+		}
+		count++; //Прочитка страницы буклета (соответсвенно переворот)
+		flag = true; //прочитали - тру
+	}
+	std::cout << count;
+	return 0;
+}
